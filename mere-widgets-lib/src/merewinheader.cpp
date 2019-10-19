2d21d9fd9d08008391cbe6e03c5df3027a9d8ef0
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2019-10-19 11:42:56 +0600

    Basic header implementation

--- mere-widgets-lib/src/merewinheader.cpp
+++ mere-widgets-lib/src/merewinheader.cpp
@@ -1,5 +1,11 @@
 #include "merewinheader.h"
 
+#include <QLabel>
+#include <QHostInfo>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QApplication>
+
 MereWinHeader::MereWinHeader(QWidget *parent) : QWidget(parent)
 {
     QPalette pal = palette();
@@ -8,6 +14,66 @@ MereWinHeader::MereWinHeader(QWidget *parent) : QWidget(parent)
     setPalette(pal);
 
     setMaximumHeight(48);
+
+    QHBoxLayout *layout = new QHBoxLayout(this);
+    layout->setContentsMargins(5, 5, 5, 5);
+
+    initUI();
+}
+
+void MereWinHeader::initUI()
+{
+    initLeftPanel();
+    initCenterPanel();
+    initRightPanel();
+}
+
+void MereWinHeader::initLeftPanel()
+{
+    QWidget *pane = new QWidget(this);
+    pane->setMaximumWidth(100);
+    layout()->addWidget(pane);
+}
+
+void MereWinHeader::initCenterPanel()
+{
+    QWidget *pane = new QWidget(this);
+    layout()->addWidget(pane);
+
+    QVBoxLayout *layout = new QVBoxLayout(pane);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(3);
+    layout->setAlignment(Qt::AlignCenter | Qt::AlignHCenter);
+
+     m_title = new QLabel("[Unknown app]");
+     m_title->setAlignment(Qt::AlignCenter | Qt::AlignHCenter);
+     m_title->setObjectName("MereWinHeaderTitle");
+     layout->addWidget(m_title);
+
+     QLabel *host = new QLabel(QString("@").append(QHostInfo::localHostName()));
+     host->setAlignment(Qt::AlignCenter | Qt::AlignHCenter);
+     layout->addWidget(host);
+}
+
+void MereWinHeader::initRightPanel()
+{
+    QWidget *pane = new QWidget(this);
+    pane->setMaximumWidth(100);
+    layout()->addWidget(pane);
+
+    QHBoxLayout *layout = new QHBoxLayout(pane);
+    layout->setAlignment(Qt::AlignRight);
+
+    QPushButton *close = new QPushButton(QIcon(":/pkg/close.png"),"");
+    close->setMaximumWidth(24);
+    close->setFlat(true);
+    layout->addWidget(close);
+    connect(close, SIGNAL(clicked(bool)), this, SLOT(close()));
+}
+
+void MereWinHeader::setTitle(const QString title)
+{
+    m_title->setText(title);
 }
 
 void MereWinHeader::mousePressEvent(QMouseEvent *event)
@@ -28,5 +94,10 @@ void MereWinHeader::mouseMoveEvent(QMouseEvent *event)
     QPoint point = event->globalPos();
     parentWidget()->move(parentWidget()->pos() + point - m_point);
     m_point = point;
+}
+
+void MereWinHeader::close()
+{
+    qApp->exit();
 }
 
--- mere-widgets-lib/src/merewinheader.h
+++ mere-widgets-lib/src/merewinheader.h
@@ -2,26 +2,39 @@
 #define MEREWINHEADER_H
 
 #include <QMouseEvent>
+#include <QLabel>
 #include <QWidget>
+#include <QDebug>
 
 class MereWinHeader : public QWidget
 {
     Q_OBJECT
 public:
     explicit MereWinHeader(QWidget *parent = nullptr);
+    void setTitle(const QString title);
 
 protected:
     void mousePressEvent(QMouseEvent *event);
     void mouseReleaseEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent *event);
 
+private:
+    void initUI();
+    void initLeftPanel();
+    void initCenterPanel();
+    void initRightPanel();
+
 signals:
 
 public slots:
+private slots:
+    void close();
 
 private:
     bool m_lock;
     QPoint m_point;
+
+    QLabel *m_title;
 };
 
 #endif // MEREWINHEADER_H
