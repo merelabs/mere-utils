73831f816b9945ad8f48a827174ea5000ba8ee4b
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2021-05-11 10:06:49 +0600

    Add string split

--- src/stringutils.h
+++ src/stringutils.h
@@ -70,6 +70,8 @@ public:
 
 
     static int indexOf(const QString& str, const QString &sub, uint occurrence = 1);
+
+    static std::vector<std::string> split(const std::string &str, char del = ' ');
 };
 
 }
