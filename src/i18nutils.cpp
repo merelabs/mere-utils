1e4576dd3d1861d5d5493d905a939444fb92f48a
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2021-08-15 06:26:37 +0600

    Code cleanup for i18n; use library methods instead

--- src/i18nutils.cpp
+++ src/i18nutils.cpp
@@ -1,120 +1,48 @@
-#include "apputils.h"
 #include "i18nutils.h"
+#include "apputils.h"
 #include "stringutils.h"
-#include "fileutils.h"
 
 #include <iostream>
-
+#include <QTranslator>
 #include <QCoreApplication>
 
-//static
 bool Mere::Utils::I18nUtils::apply()
 {
+    QLocale fallback("en");
+    bool ok = apply(fallback);
+    if(!ok)
+        std::cout << "WARN:: Could not find fallback translator resource for  " << fallback.bcp47Name().toStdString() << std::endl;
+
     QLocale locale = QLocale::system();
+
     return apply(locale);
 }
 
 //static
-bool Mere::Utils::I18nUtils::apply(QLocale locale)
+bool Mere::Utils::I18nUtils::apply(const QLocale &locale)
 {
     QTranslator *translator = new QTranslator();
-    return apply(translator, locale);
-}
 
-//static
-bool Mere::Utils::I18nUtils::apply(QTranslator *translator)
-{
-    QLocale locale = QLocale::system();
-    return apply(translator, locale);
-}
-
-//static
-bool Mere::Utils::I18nUtils::apply(QTranslator *translator, QLocale locale)
-{
-    QCoreApplication::removeTranslator(translator);
-
-    std::string path = i18nFile(locale);
-    if (Mere::Utils::StringUtils::isBlank(path))
-        return false;
-
-    bool ok = translator->load(path.c_str());
+    bool ok = translator->load(locale, "lock", "_", QString::fromStdString(i18nPath()));
     if (!ok) return false;
 
-    std::cout << "Applying following translator resource: " << path << std::endl;
-
+    std::cout << "Applying following translator resource: " << translator->filePath().toStdString() << std::endl;
     QCoreApplication::installTranslator(translator);
 
-    return true;
+    return ok;
 }
 
-//static
-bool Mere::Utils::I18nUtils::exist(QLocale locale)
-{
-    QString name = locale.name();
-
-    std::string pattern = i18nPattern();
-    QString path = QString(pattern.c_str()).arg(name);
-
-    return FileUtils::isExist(path);
-}
-
-//static
-std::string Mere::Utils::I18nUtils::i18nFile(QLocale locale)
-{
-    QString name = locale.name();
-
-    std::string pattern = i18nPattern();
-    QString path = QString(pattern.c_str()).arg(name);
-
-    return FileUtils::isExist(path) ? path.toStdString() : fallback(locale);
-}
-
-//static
-std::string Mere::Utils::I18nUtils::i18nPattern()
-{
-    std::string path = i18nPath();
-    return path.append(Mere::Utils::AppUtils::appCode()).append("_%1.qm");
-}
-
-//static
 std::string Mere::Utils::I18nUtils::i18nPath()
 {
     return i18nMerePath().append(Mere::Utils::AppUtils::appCode().c_str()).append("/").append("i18n/");
 }
 
-//static
 std::string Mere::Utils::I18nUtils::i18nMerePath()
 {
     return i18nBasePath().append("mere/");
 }
 
-//static
 std::string Mere::Utils::I18nUtils::i18nBasePath()
 {
     return "/usr/local/share/";
-}
-
-//static
-std::string Mere::Utils::I18nUtils::fallback(QLocale locale)
-{
-    QString pattern = QString(i18nPattern().c_str());
-
-    std::vector<std::string> paths {
-            pattern.arg(locale.name()).toStdString(),
-            pattern.arg(locale.bcp47Name()).toStdString(),
-            pattern.arg("en_US").toStdString(),
-            pattern.arg("en").toStdString()
-    };
-
-    for(const std::string &path : paths)
-    {
-        if (FileUtils::isExist(path))
-            return path;
-
-        std::cout << "WARN: Failed to find i18n resource file for fallback - " << path << std::endl;
-    }
-
-    std::cout << "Unable to find i18n resource file" << std::endl;
-
-    return "";
 }
--- src/i18nutils.h
+++ src/i18nutils.h
@@ -2,20 +2,10 @@
 #define MERE_UTILS_I18NUTILS_H
 
 #include "global.h"
-#include <QTranslator>
 #include <QLocale>
 
 namespace Mere
 {
-    namespace Utils
-    {
-//        const QString i18nPathPattern = "/usr/local/share/mere/mere-about/i18n/mere-about_%1.qm";
-    }
-}
-
-namespace Mere
-{
-
 namespace Utils
 {
 
@@ -26,20 +16,14 @@ private:
 
 public:
     static bool apply();
-    static bool apply(QLocale locale);
-    static bool apply(QTranslator *translator);
-    static bool apply(QTranslator *translator, QLocale locale);
+    static bool apply(const QLocale &locale);
 
 private:
-    static bool exist(QLocale locale);
-    static std::string i18nFile(QLocale locale);
     static std::string i18nPath();
     static std::string i18nMerePath();
     static std::string i18nBasePath();
-
-    static std::string i18nPattern();
-    static std::string fallback(QLocale locale);
 };
+
 }
 }
 #endif // MERE_UTILS_I18NUTILS_H
